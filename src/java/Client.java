645bf14dd14eddbc6d00ba389a360d3820acce4d
EvanWallSaintPaul <evanw.mn@gmail.com>
2018-02-07 14:59:05 -0600

    add FileSplitter classwhich splits the file into bytes.

--- src/java/Client.java
+++ src/java/Client.java
@@ -1,11 +1,16 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+
 
 public class Client {
     private final static int PORT = 13;
     private static final String HOSTNAME = "localhost";
-
+    private FileSplitter _fileSplitter = new FileSplitter();
+    
    public Client() {
        Runnable r = new Runnable() {
            @Override
@@ -25,20 +30,36 @@ private void runWork() {
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
 
-        System.out.println("Enter a sentence to send to server: " );
-        String sentence = inFromUser.nextLine();
+        System.out.println("Enter a file in src to send to server: " );
+        //String filePath = inFromUser.nextLine();
+        String filePath = "/Users/evanwall/Desktop/ICS460/proj1/ics460project1/fileToSend.txt";
+    
+        // convert file to byte[]
+        byte[] bFile = _fileSplitter.readBytesFromFile(filePath);
 
-        sendData = sentence.getBytes();
-        DatagramPacket sendPacket =
-            new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-        clientSocket.send(sendPacket);
-        DatagramPacket receivePacket =
-            new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("CLIENT: FROM SERVER:" + modifiedSentence);
+        //java nio
+        //byte[] bFile = Files.readAllBytes(new File("C:\\temp\\testing1.txt").toPath());
+        //byte[] bFile = Files.readAllBytes(Paths.get("C:\\temp\\testing1.txt"));
+
+        // save byte[] into a file
+        Path path = Paths.get("C:\temp\\test2.txt");
+        Files.write(path, bFile);
+
+        System.out.println("Done");
+
+        //Print bytes[]
+        for (int i = 0; i < bFile.length; i++) {
+            sendData = bFile;
+            DatagramPacket sendPacket =
+                new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+            DatagramPacket receivePacket =
+                new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence = new String(receivePacket.getData());
+            System.out.println("CLIENT: FROM SERVER:" + modifiedSentence);
+        }
         clientSocket.close();
-
     } catch ( IOException ex ) {
         ex.printStackTrace();
     }
---
+++ src/java/FileSplitter.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+// source https://www.mkyong.com/java/how-to-convert-file-into-an-array-of-bytes/
+public class FileSplitter {
+
+    public byte[] readBytesFromFile(String filePath) {
+
+        FileInputStream fileInputStream = null;
+        byte[] bytesArray = null;
+
+        try {
+
+            File file = new File(filePath);
+            bytesArray = new byte[(int) file.length()];
+
+            //read file into bytes[]
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytesArray);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        return bytesArray;
+
+    }
+
+}
